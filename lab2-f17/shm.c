3683214663488c9667a42f64e3bd50557778ae09
Alexander Ku <aku006@sledge.cs.ucr.edu>
2020-12-17 21:54:09 -0800

    shm_open increments refcnt only on existing page, initializes refcnt with 1 on new pages

--- lab2-f17/shm.c
+++ lab2-f17/shm.c
@@ -41,35 +41,40 @@ int shm_open(int id, char **pointer) {
   
   // Case 1: The target ID already exists
   // First loop through the entire shm table to see if ID exists or not
+  // If the ID exists, then Case 1 is fulfilled, and the next part is skipped
   for(i= 0; i < 64; ++i)
   {
-	// If the ID does exist, then set exist to 1 and break
+	// If the ID does exist, then set exist to 1, increment refcnt, and break
   	if(shm_table.shm_pages[i].id == id)
         {
 	    exist = 1;
+            shm_table.shm_pages[i].refcnt++;
 	    break;
 	}
   } 
 
   // Case 2: The target ID does not exist in the table
-  // Run this process in that case
+  // Only run this process if the above loop does not return true for char exist
   if(!exist)
   {
       // Loop through the table to find the page where the shared memory segment does not exist
       for(i = 0; i < 64; ++i)
       {
 	  // If found, allocate a new page and then break
+	  // Also set its initial refcnt to 1
           if(shm_table.shm_pages[i].id == 0)
           {
 		char *page = kalloc();
                 memset(page, 0, PGSIZE);
                 shm_table.shm_pages[i].frame = page;
                 shm_table.shm_pages[i].id = id;
+                shm_table.shm_pages[i].refcnt = 1;
                 break;
           }
       }
   }
   // Adjust the memory table and add the ID to its corresponding memory page
+  // Needs to do this regardless of whether the page initally existed or not
   mappages(
 	myproc()->pgdir,
 	(void *)PGROUNDUP(myproc()->sz),
@@ -77,11 +82,10 @@ int shm_open(int id, char **pointer) {
         V2P(shm_table.shm_pages[i].frame),
         (PTE_W | PTE_U)
   );
-  shm_table.shm_pages[i].refcnt++;
   *pointer = (char *)PGROUNDUP(myproc()->sz);
   myproc()->sz += PGSIZE;
-  release(&(shm_table.lock));
 
+  release(&(shm_table.lock)); //Need to release the lock before exiting!
   return 0;
 }
 
@@ -92,7 +96,7 @@ int shm_close(int id) {
   //you write this too!
 
   int i = 0;
-  acquire(&(shm_table.lock)); 
+  acquire(&(shm_table.lock)); // Acquire the lock for shm_table
  
   // Loop through the page table first to look for the ID
   for(i = 0; i < 64; ++i)
@@ -106,7 +110,7 @@ int shm_close(int id) {
  	       --shm_table.shm_pages[i].refcnt;
 	  }
 
-    	  // Set the values to zero otherwise
+    	  // Set the values of the page table to zero otherwise
 	  else 
 	  {
               shm_table.shm_pages[i].id = 0;
@@ -116,7 +120,6 @@ int shm_close(int id) {
 	  break;
       }
   }
-  release(&(shm_table.lock)); 
-
+  release(&(shm_table.lock)); // Need to release the lock before exiting!
   return 0;
 }
